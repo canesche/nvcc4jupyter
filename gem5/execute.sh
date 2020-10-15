cd334a7275420297bb32acc5e8a116d27ac48dca
canesche <michael.canesche@gmail.com>
2020-10-15 09:42:24 -0300

    update

--- gem5/execute.sh
+++ gem5/execute.sh
@@ -1,4 +1,3 @@
 # exemplo de execução de um Hello World para X86
-LOCAL_GEM5='/content/gdrive/My\ Drive/gem5'
-chmod +x $LOCAL_GEM5/build/$1/gem5.opt
-$LOCAL_GEM5/build/$1/gem5.opt $2
+chmod +x '/content/gdrive/My\ Drive/gem5'/build/$1/gem5.opt
+'/content/gdrive/My\ Drive/gem5'/build/$1/gem5.opt $2
