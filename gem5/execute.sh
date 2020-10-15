5c5562ac1b686864bcaf1000e1de9524960192e9
canesche <michael.canesche@gmail.com>
2020-10-15 09:56:44 -0300

    gem5 features

--- gem5/execute.sh
+++ gem5/execute.sh
@@ -1,3 +1,3 @@
 # exemplo de execução de um Hello World para X86
-chmod +x '/content/gdrive/My\ Drive/gem5'/build/$1/gem5.opt
-'/content/gdrive/My\ Drive/gem5'/build/$1/gem5.opt $2
+chmod +x '/content/gdrive/My Drive/gem5'/build/$1/gem5.opt
+'/content/gdrive/My Drive/gem5'/build/$1/gem5.opt $2
