60a0b6cb431fc02b1bcc16d145197617a844f1f3
canesche <michael.canesche@gmail.com>
2020-10-25 22:02:35 -0300

    new features valgrind

--- valgrind/execute.sh
+++ valgrind/execute.sh
@@ -1,3 +1,3 @@
-valgrind --tool=cachegrind --D1=32768,2,32 --I1=32768,2,32 --LL=65536,2,32 ./valgrind_code.out
+valgrind --tool=cachegrind --D1=32768,2,32 --I1=32768,2,32 --LL=65536,2,32 /content/valgrind_code.out
 cat /content/cachegrind*
 rm /content/cachegrind*
