9fb90f367457643523d1fa317058247a2d09f544
canesche <michael.canesche@gmail.com>
2020-10-25 21:06:50 -0300

    new features valgrind

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ setup(
     version='0.0.3',
     author='Andrei Nechaev, M. Canesche',
     author_email='lyfaradey@yahoo.com, michael.canesche@gmail.com',
-    py_modules=['nvcc_plugin', 'v2.v2', 'v1.v1', 'c.c', 'cpp.cpp', 
-        'verilog.verilog', 'java.java', 'gem5.gem5', 'valgrind.valgrind'
-        'common.helper'],
+    py_modules=['nvcc_plugin', 'v2.v2', 'v1.v1', 'c.c', 'cpp.cpp', 'verilog.verilog', 'java.java', 'gem5.gem5', 'valgrind.valgrind', 'common.helper'],
     url='htpps://github.com/canesche/nvcc4jupyter',
     license='LICENSE',
     description='Jupyter notebook plugin to run CUDA C/C++, GCC code',
