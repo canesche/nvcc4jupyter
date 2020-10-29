1f8048cef223ef689540178995ee5fa5dda0885d
canesche <michael.canesche@gmail.com>
2020-10-29 16:53:14 -0300

    update

--- gem5/gem5.py
+++ gem5/gem5.py
@@ -46,11 +46,100 @@ class Gem5Plugin(Magics):
                     output = subprocess.check_output(args, stderr=subprocess.STDOUT)
                     output = output.decode('utf8')
                     helper.print_out(output.replace("\n\n","\n"))
+
+    def view_scope(self):
+        import ipywidgets as widgets
+        from IPython.display import display
+
+        from ipywidgets import *
+
+        def on_button_clicked(b):
+            print("Button clicked.", b.description)
+
+        def on_value_change(change):
+            print(change['owner'].name)
+
+        def create_expanded_button(description, button_style=""):
+            btn = Button(description=description, button_style=button_style, layout=Layout(height='auto', width='auto'))
+            btn.on_click(on_button_clicked)
+            return btn
+
+        def create_expanded_slider(id, description="", button_style="", min=1, max=10, value=1, step=1):
+            slider = IntSlider(description="", button_style=button_style, layout=Layout(height='auto', width='auto'), min=min, max=max, value=value, step=step)
+            slider.observe(on_value_change, names='value')
+            slider.name = id
+            return slider
+        def create_expanded_intText(description="", button_style="", min=1, max=10, value=1, step=1):
+            return BoundedIntText(description=description, button_style=button_style, layout=Layout(height='auto', width='auto'), min=min, max=max, value=value, step=step)
+        def create_expanded_floatText(description="", button_style="", min=1, max=10, value=1, step=1):
+            return BoundedFloatText(description=description, button_style=button_style, layout=Layout(height='auto', width='auto'), min=min, max=max, value=value, step=step)
+        def create_expanded_Select(description="", button_style="", value=0, options=[]):
+            return Select(description=description, button_style=button_style, layout=Layout(height='40px', width='auto'), value=value, options=options)
+
+        # create a 10x2 grid layout
+        grid = GridspecLayout(2, 10)
+        # fill it in with widgets
+        grid[0, 0] = create_expanded_button("Architecture", "warning")
+        grid[0, 1] = create_expanded_button("X86")
+        grid[0, 2] = create_expanded_button("RiscV")
+        grid[0, 3] = create_expanded_button("Arm")
+        grid[1, 0] = create_expanded_button("CPU", "warning")
+        grid[1, 1] = create_expanded_button("Simple")
+        grid[1, 2] = create_expanded_button("In Order")
+        grid[1, 3] = create_expanded_button("Out Order")
+
+        gridclock = GridspecLayout(1, 10)
+        gridclock[0,0] = create_expanded_button("Clock (GHz)", "warning")
+        gridclock[0,1] = create_expanded_floatText(value=2.0, min=0.1, max=5.0, step=1)
+
+        gridCache = GridspecLayout(2, 10)
+        gridCache[0,0] = create_expanded_button("Cache", "warning")
+        gridCache[0,1] = create_expanded_button("Size (kB)", "warning")
+        gridCache[0,2] = create_expanded_button("Associative", "warning")
+        #gridCache[0,3] = create_expanded_button("tag_latency", "warning")
+        gridCache[0,3] = create_expanded_button("data_latency", "warning")
+        #gridCache[0,4] = create_expanded_button("resp_latency", "warning")
+        gridCache[0,4] = create_expanded_button("mshrs", "warning")
+        gridCache[0,5] = create_expanded_button("tgts_per_mshr", "warning")
+        gridCache[1,0] = create_expanded_button("L1Cache", "warning")
+        gridCache[1,1] = create_expanded_intText(value=16, max=2048, step=2)
+        gridCache[1,2] = create_expanded_slider("assoc", value=2, min=2, max=10, step=2)
+        #gridCache[1,3] = create_expanded_slider(value=2)
+        gridCache[1,3] = create_expanded_slider("data_latency", value=2)
+        #gridCache[1,4] = create_expanded_slider(value=2)
+        gridCache[1,4] = create_expanded_slider("mshrs", value=4)
+        gridCache[1,5] = create_expanded_slider("tgts_per_mshr", value=20, min=10, max=50)
+
+        gridCacheL2 = GridspecLayout(1, 10)
+
+        gridCacheL2[0,0] = create_expanded_button("L2Cache", "warning")
+        gridCacheL2[0,1] = create_expanded_floatText(value=256, max=2048, step=2)
+        gridCacheL2[0,2] = create_expanded_slider("", value=8)
+        #gridCacheL2[0,3] = create_expanded_slider(value=20, max=40)
+        gridCacheL2[0,3] = create_expanded_slider("", value=20, max=40)
+        #gridCacheL2[0,4] = create_expanded_slider(value=20, max=40)
+        gridCacheL2[0,4] = create_expanded_slider("", value=20, max=40)
+        gridCacheL2[0,5] = create_expanded_slider("", value=12, min=10, max=50)
+
+        gridMemory = GridspecLayout(1, 10)
+        gridMemory[0,0] = create_expanded_button("Memory", "warning")
+        gridMemory[0,1] = create_expanded_Select("", options=['DDR3_1600_8x8','DDR4_2400_8x8'], value='DDR3_1600_8x8')
+
+        display(grid)
+        display(gridclock)
+        print("")
+        display(gridCache)
+        display(gridCacheL2)
+        print("")
+        display(gridMemory)
     
     @cell_magic
-    def gem5(self, line, cell="#"):
+    def gem5(self, line, cell):
         args = line.split()
-        print(args)
+        
+        if 'view' in line:
+            self.view_scope()
+            return
 
         file_path = '/content/gem5_code'
 
