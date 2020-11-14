65a75576a7adf85317cf09c04dc82a2876c85fa5
canesche <michael.canesche@gmail.com>
2020-11-14 15:45:19 -0300

    update

--- gem5/gem5.py
+++ gem5/gem5.py
@@ -141,7 +141,7 @@ class Gem5Plugin(Magics):
             gridCache[0,2] = create_Text("Associative", "warning")
             gridCache[0,3] = create_Text("data_latency", "warning")
             gridCache[1,0] = create_Text("L1Cache", "warning")
-            gridCache[1,1] = create_Dropdown("size_l1", options=opts[3:10], value=16)
+            gridCache[1,1] = create_Dropdown("size_l1", options=opts[0:10], value=16)
             gridCache[1,2] = create_Dropdown("assoc_l1", options=opts[:5], value=2)
             gridCache[1,3] = create_Dropdown("latency_l1", options=range(14,28), value=16)
             gridCache[2,0] = create_Text("L2Cache", "warning")
